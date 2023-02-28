b4870fc397320ccedc5a74dbe0ed173bfaa19727
filebin <alex.mousse9@gmail.com>
2023-02-28 22:40:16 +0300

    copy-button

--- index.js
+++ index.js
@@ -20,3 +20,11 @@ function ready() {
         observer.observe(text);
     });
 }
+
+const copyBtn = document.querySelector("#copy-ip-btn");
+
+const ipText = document.querySelector(".ip-address");
+
+copyBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(ipText.textContent);
+});
