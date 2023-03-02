c56d6fd944a44227ae120345f8de5f6a07191633
filebin <alex.mousse9@gmail.com>
2023-03-02 16:37:29 +0300

    favicon added, social nets added

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 // Создать наблюдателя
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        const texts = [entry.target];
+        const texts = entry.target.querySelectorAll(".animation-left");
         //const texts = entry.target.querySelectorAll(".square");
         if (entry.isIntersecting) {
-            texts.forEach(text => { text.classList.add("text-animation"); });
+            const time = 200;
+            var t = 0;
+            texts.forEach(text => {
+                setTimeout(() => { text.classList.add("animation-center") }, t);
+                t += time;
+            });
             return;
         }
         // перемещение завершено, теперь надо удалить класс
-        texts.forEach(text => { text.classList.remove("text-animation"); });
+        texts.forEach(text => { text.classList.remove("animation-center"); });
     });
 });
 
 document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
-    const texts = document.querySelectorAll(".animated-text");
+    const texts = document.querySelectorAll(".animation-trigger");
     texts.forEach(text => {
         observer.observe(text);
     });
