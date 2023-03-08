7dd67b963096799fef435540ec9dc520a290ed9b
filebin <alex.mousse9@gmail.com>
2023-03-08 17:23:44 +0300

    added bootstrap and screenshots

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__, static_folder="static")
 app.config["TRAP_HTTP_EXCEPTIONS"] = True
-
 
 @app.errorhandler(Exception)
 def error_handler(error):
@@ -11,7 +11,12 @@ def error_handler(error):
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    screenshots_folder = "static/img/screenshots/"
+    files = os.listdir(screenshots_folder)
+    filter(lambda f: f.endswith(".png"), files)
+    files = [ "/" + os.path.join(screenshots_folder, f) for f in files ]
+    files.sort()
+    return render_template("index.html", screenshots=files)
 
 
 @app.route("/donate")
